3a778504aeeff94bf10a0d85bfc40d19f8f73a1b
hellvn <vuongpt93@gmail.com>
2021-01-02 01:36:43 +0700

    Lab2

---
+++ C_Sharp_Lab2/SS5/Snippet08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] classOne = { "Alan", "Chris", "Monica" };
+            string[] classTwo = { "Katie", "Niel", "Mark" };
+            Console.WriteLine("students of class I:\tStudents of Class II");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(classOne[i] + "\t\t\t" + classTwo[i]);
+            }
+            classTwo = classOne;
+            Console.WriteLine("\nStudents of classII after referencing classI: ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(classTwo[i] + "");
+
+
+            }
+            Console.WriteLine();
+            classTwo[2] = "Mike";
+            Console.WriteLine("Students of class I after changing the third students in class II:   ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(classOne[i] + "");
+            }
+        }
+    }
+}
---
+++ C_Sharp_Lab2/SS5/Snippet12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array objArray = Array.CreateInstance(typeof(string), 5);
+            objArray.SetValue("Marketing", 0);
+            objArray.SetValue("Finace", 1);
+            objArray.SetValue("Human Resources", 2);
+            objArray.SetValue("Information Technology", 3);
+            objArray.SetValue("Bussiness Adimnistration", 4);
+            for (int i = 0; i <= objArray.GetUpperBound(0); i++)
+            {
+                Console.WriteLine(objArray.GetValue(i));
+            }
+        }
+    }
+}
