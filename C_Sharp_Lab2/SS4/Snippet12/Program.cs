67c8a0f20c455cbccea83584f4fb61c415a131f6
hellvn <vuongpt93@gmail.com>
2021-01-01 21:14:51 +0700

    Lab2

---
+++ C_Sharp_Lab2/SS4/Snippet12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 2;
+            int columns = 2;
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++);
+            }
+            Console.WriteLine();
+        }
+    }
+}
