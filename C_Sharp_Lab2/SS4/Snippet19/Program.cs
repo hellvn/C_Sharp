171ce99d62a817d55f29aff2afb671b2c1279478
hellvn <vuongpt93@gmail.com>
2021-01-02 00:25:22 +0700

    Lab2

---
+++ C_Sharp_Lab2/SS4/Snippet19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = 17;
+            int numtwo = 2;
+
+            while (numtwo <= numOne - 1)
+            {
+                if (numOne % numtwo == 0)
+                {
+                    Console.WriteLine("Not a prime number");
+                    break;
+                }
+                numtwo++;
+
+                if (numtwo == numOne)
+                {
+                    Console.WriteLine("Prime number");
+                }
+            }
+        }
+    }
+}
