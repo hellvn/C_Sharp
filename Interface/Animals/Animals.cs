37f3a98cc4d0780a1f60685135eb4e2d3bb5c0ed
hellvn <vuongpt93@gmail.com>
2021-01-20 11:24:20 +0700

    CSDBC

---
+++ Interface/Animals/Animals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    interface IAnimal
+    {
+        void Habitat();
+    }
+    class Thang : IAnimal
+    {
+        public void Habitat()
+        {
+            Console.WriteLine("Can be housed with human beings");
+        }
+        static void Main(string[] args)
+        {
+            Thang objThang = new Thang();
+            Console.Write(objThang.GetType().Name+" ");
+            objThang.Habitat();
+        }
+    }
+}
