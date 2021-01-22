39e043a32cf03129b72f179888373c9e5b52a29e
hellvn <vuongpt93@gmail.com>
2021-01-22 11:16:01 +0700

    Practice

--- practice/GeometryExample/Cylinder.cs
+++ practice/GeometryExample/Cylinder.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GeometryExample
 {
@@ -10,42 +6,34 @@ namespace GeometryExample
     {
         public double Radius;
         public double Height;
+        public double BArea;
+        public double LArea;
+        public double TArea;
+        public double Volume;
+
+        public void Process()
+        {
+            BArea = (Radius * Radius * Math.PI);
+            LArea = 2 * Math.PI * Radius * Height;
+            TArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+        public void Result()
+        {
+            Console.WriteLine("\nCylinder Characteristics");
+            Console.WriteLine("Radius: {0}, Height: {1}", Radius, Height);
+            Console.WriteLine("Base: {0:f} | Lateral: {1:f} | Total: {2:f} | Volume: {3:f}", BArea , LArea, TArea, Volume);
+        }
         public static void Main(string[] args)
         {
-            Cylinder objCylinder = new Cylinder();
+            Cylinder cylinder = new Cylinder();
             Console.WriteLine("Enter the dimenstions of cylinder");
             Console.Write("Radius: ");
-            objCylinder.Radius = Convert.ToDouble(Console.ReadLine());
+            cylinder.Radius = Convert.ToDouble(Console.ReadLine());
             Console.Write("Height: ");
-            objCylinder.Height = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nCylinder Characteristics");
-            Console.WriteLine("Radius: {0}, Height: {1}", objCylinder.Radius, objCylinder.Height);
-            Console.WriteLine("Base: {0:f} | Lateral: {1:f} | Total: {2:f} | Volume: {3:f}", objCylinder.BaseArea(), objCylinder.LateralArea(), objCylinder.TotalArea(), objCylinder.Volume());
+            cylinder.Height = Convert.ToDouble(Console.ReadLine());
+            cylinder.Process();
+            cylinder.Result();
         }
-        public double BaseArea()
-        {
-            double BArea;
-            BArea = (Radius * Radius * Math.PI);
-            return BArea;
-        }
-        public double LateralArea()
-        {
-            double LArea;
-            LArea = 2 * Math.PI * Radius * Height;
-            return LArea;
-        }
-        public double TotalArea()
-        {
-            double TArea;
-            TArea = 2 * Math.PI * Radius * (Height + Radius);
-            return TArea;
-        }
-        public double Volume()
-        {
-            double Volume;
-            Volume = Math.PI * Radius * Radius * Height;
-            return Volume;
-        }
-        
     }
 }
